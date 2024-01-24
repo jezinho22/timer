5293f193b26e291aafa14d23ecb09f38e1f45fe4
jezinho <jez.johns@gmail.com>
2024-01-24 12:26:21 +0000

    select back image

--- src/TimeForm.jsx
+++ src/TimeForm.jsx
@@ -3,8 +3,19 @@ export default function TimeForm({handleSubmit, handleChange}) {
 
   return (
     <form id="time-input" onSubmit={handleSubmit}>
-      <input type="text" id="mins" name="mins" placeholder="How many minutes?" onChange={handleChange}/>
-      <input type="submit" />
+      <div>
+        <input type="text" id="mins" name="mins" placeholder="How many minutes?" onChange={handleChange}/>
+        <input type="submit" />   
+      </div>
+      <div>
+      <label htmlFor="bground">Change background</label>   
+      <select name="bground" id="bground" defaultValue={"coffee1.jpg"}>
+        <option value="coffee1.jpg">Coffee 1</option>
+        <option value="coffee2.jpg">Coffee 2</option>
+        <option value="tea1.jpg">Tea 1</option>
+        <option value="tea2.jpg">Tea 2</option>
+      </select>
+      </div>
     </form>
   )
 }
